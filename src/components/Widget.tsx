cde0818728f86803e0ad22a91566b0391a207c1a
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-03 18:28:26 -0300

    feat: widget form component and fixes on widget button

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react';
+import { WidgetForm } from './WidgetForm';
 
 export const Widget = () => {
   return (
-    <Popover className={'absolute bottom-5 right-5'}>
-      <Popover.Panel>Hello world</Popover.Panel>
+    <Popover
+      className={
+        'absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'
+      }
+    >
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
 
       <Popover.Button
         className={
