338a0f4b67ef24463a5aefbb987703627dab3e14
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-03 19:26:01 -0300

    feat: feedback content component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,3 +1,37 @@
-export const FeedbackContentStep = () => {
-  return <p>FeedbackContentStep</p>;
+import { ArrowLeft } from 'phosphor-react';
+import { FeedbackType, feedbackTypes } from '..';
+import { CloseButton } from '../../CloseButton';
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+
+export const FeedbackContentStep = ({
+  feedbackType,
+  onFeedbackRestartRequested,
+}: FeedbackContentStepProps) => {
+  const { title, image } = feedbackTypes[feedbackType];
+
+  return (
+    <>
+      <header>
+        <button
+          type={'button'}
+          className={'top-5 left-5 absolute text-zinc-400 hover:text-zinc-100'}
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight={'bold'} className={'w-4 h-4'} />
+        </button>
+
+        <span className={'text-xl leading-6 flex items-center gap-2'}>
+          <img src={image.source} alt={image.alt} className={'w-6 h-6'} />
+          {title}
+        </span>
+        <CloseButton />
+      </header>
+
+      <div className={'flex py-8 gap-2 w-full'}></div>
+    </>
+  );
 };
